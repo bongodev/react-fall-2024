2b22a98bb12a45244af18bf9f8664c526c708b2b
kmtusher97 <kmtusher97@gmail.com>
2025-01-19 22:25:01 +0600

    Add light and dark theme using context and custom hooks

--- src/components/project/Projects.jsx
+++ src/components/project/Projects.jsx
@@ -1,12 +1,22 @@
+import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
 import { ProjectPropType } from '../propTypesDefs';
 
+import { useTheme } from '../../hooks';
+
 import { Project } from './Project';
 
 export const Projects = ({ projects }) => {
+  const { isDarkMode } = useTheme();
+
   return (
-    <div className="container space-y-6">
+    <div
+      className={clsx(
+        'container space-y-6',
+        isDarkMode ? 'app-dark' : 'app-light'
+      )}
+    >
       {projects.map((project) => (
         <Project key={project.name} project={project} />
       ))}
